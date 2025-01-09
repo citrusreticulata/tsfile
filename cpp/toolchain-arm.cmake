30d3335a31ac70f63031e4fad56b539341eaf3b2
橘子 <70060096+citrusreticulata@users.noreply.github.com>
2025-01-09 18:05:24 +0800

    改cmake，支持静态编译，禁用测试
    改cmake，支持静态编译，禁用测试。直接在主cmakelists中禁用测试，修改src/下的cmake来支持静态编译，并且支持输出libtsfile.a

--- cpp/toolchain-arm.cmake
+++ cpp/toolchain-arm.cmake
@@ -3,39 +3,24 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR arm)
 
-# cyber7001交叉编译链 gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi.zip
-# set(tools /home/ubuntu/software/gcc-linaro-5.5.0-2017.10-x86_64_arm-linux-gnueabi)
-# set(CMAKE_C_COMPILER ${tools}/bin/arm-linux-gnueabi-gcc)
-# set(CMAKE_CXX_COMPILER ${tools}/bin/arm-linux-gnueabi-g++)
-# set(CMAKE_FIND_ROOT_PATH ${tools}/lib)
-
-# 更新一点的 TQ3588-gcc-linaro-11.3.0.tar.bz2
-# 2025.01.06记录. 可以初步交叉编译，能过大小端、gcc版本等检查
-set(tools /home/ubuntu/software/TQ3588-gcc-linaro-11.3.0/toolchain)
-set(CMAKE_C_COMPILER ${tools}/bin/aarch64-linux-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/aarch64-linux-g++)
-set(CMAKE_FIND_ROOT_PATH ${tools}/lib)
-include_directories(${tools}/include)
-# include_directories(${tools}/include/libuuid)
-
 # 自己下载的, 最终使用的是13.3.rel1 ===== !! =====
 # 2025.01.06记录. 似乎交叉编译的uuid库还是找不到
-# set(tools /home/ubuntu/software/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-linux-gnueabihf)
-# set(CMAKE_C_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-gcc)
-# set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-g++)
-# set(CMAKE_FIND_ROOT_PATH ${tools}/lib)
-# include_directories(${tools}/include)
+set(tools /home/ubuntu/software/arm-gnu-toolchain-13.3.rel1-x86_64-arm-none-linux-gnueabihf)
+set(CMAKE_C_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-linux-gnueabihf-g++)
+set(CMAKE_FIND_ROOT_PATH ${tools}/lib)
+message(STATUS "CMAKE_FIND_ROOT_PATH= ${CMAKE_FIND_ROOT_PATH}")
+include_directories(${tools}/include)
 # include_directories(${tools}/include/libuuid)
+# find_library(uuid NAMES uuid PATHS ${CMAKE_FIND_ROOT_PATH} NO_DEFAULT_PATH REQUIRED)
+# message(STATUS "Found tsfile library: ${uuid}")
+# message(STATUS "UUID_LIBRARIES: ${UUID_LIBRARIES}")
+# message(FATAL_ERROR "UUID_LIBRARY_DIRS: ${UUID_LIBRARY_DIRS}")
+
 
 # set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 # set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 # set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
-
-# 自己下载的
-# set(tools /home/ubuntu/software/arm-gnu-toolchain-11.3.rel1-x86_64-arm-none-eabi)
-# set(CMAKE_C_COMPILER ${tools}/bin/arm-none-eabi-gcc)
-# set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-eabi-g++)
-# set(CMAKE_FIND_ROOT_PATH ${tools}/lib)
 
 # set(CMAKE_CXX_STANDARD 11)  # 强制使用 C++11 标准
 # set(CMAKE_CXX_STANDARD_REQUIRED ON)
